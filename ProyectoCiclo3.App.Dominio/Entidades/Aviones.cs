a265ea0536afe136c20247603e7f1a3ecb594c65
Crisdav <https://github.com/Crisdav/misiontic.git>
2021-09-22 16:04:51 -0500

    first commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aviones{
+        public int id { get; set; }
+        public string modelo { get; set; }
+        public int num_asientos { get; set; }
+        public int num_baños { get; set; }
+        public int cap_max_peso { get; set; }
+    }
+ 
+}
